27b02f459006a6ca284b4a7319173fdba66d252f
Kheir Eddine MEDKOUR <kmedkour@amana.local>
2020-08-03 10:51:14 +0100

    first commit

---
+++ src/main/java/com/example/geestiondestockserveur/GestionDeStockServeurApplication.java
@@ -0,0 +1,13 @@
+package com.example.geestiondestockserveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStockServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeStockServeurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geestiondestockserveur/GestionDeStockServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geestiondestockserveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
